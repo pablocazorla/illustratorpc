149b053ade366437c626bedb1756aa10e5c3c0ce
Pablo Cazorla <pablo.david.cazorla@gmail.com>
2013-03-11 23:34:36 -0300

    with portfolio

--- portfolio.php
+++ portfolio.php
@@ -5,25 +5,25 @@ Template Name: Portfolio
 ?>
 
 <?php get_header(); ?>
-
-<h1>Portfolio</h1>
-
-
-<?php query_posts('posts_per_page=60'); while ( have_posts() ) : the_post(); ?>
-    
-    
-    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><br><br>
-   
-<?php endwhile; wp_reset_query(); ?>
-
-
-
-
-
-
-
-
-
-
-
+<article class="portfolio">
+	<section class="summary">
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		<?php the_content(); ?>
+		<?php endwhile; endif; ?>
+	</section>
+	<section class="content">
+		<div id="gallery">
+			<?php query_posts('posts_per_page=60'); while ( have_posts() ) : the_post(); ?>	    
+			<figure>
+				<a href="<?php the_permalink(); ?>" class="explain-work" rel="<?php the_ID();?>"></a>				
+					<?php if(has_post_thumbnail()): the_post_thumbnail('thumbnail'); endif; ?>										
+				<figcaption>
+					<h2><?php the_title(); ?></h2>
+					<div class="categories"><?php the_category(', '); ?></div>
+				</figcaption>						
+			</figure>	   
+			<?php endwhile; wp_reset_query(); ?>
+		</div>
+	</section>
+</article>
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -3,12 +3,12 @@
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	
 	<article class="work">
-		<section class="description">
+		<section class="summary">
 			<h1><?php the_title(); ?></h1>
-			<?php the_category(); ?>
+			<div class="categories"><?php the_category(', '); ?></div>
 			<?php the_excerpt(); ?>
 		</section>
-		<section class="image">
+		<section class="content">
 			<?php the_content(); ?>
 		</section>
 		
